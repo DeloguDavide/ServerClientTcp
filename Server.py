ed9d3319c1d3610cb828e9f82500112420573237
Davide Delogu <davide.delogu@itiscuneo.eu>
2024-09-27 13:25:11 +0200

    ServerClientTcpMovimentiFunzionante

--- Server.py
+++ Server.py
@@ -1,44 +1,34 @@
 import socket as s
 
-server_tcp=s.socket(s.AF_INET, s.SOCK_STREAM) 
+server_tcp = s.socket(s.AF_INET, s.SOCK_STREAM)
 
 server_tcp_address = ("10.210.0.164", 12345)
 
 server_tcp.bind(server_tcp_address)
 
 server_tcp.listen(1)
-
 try:
     while True:
-
-        client,address=server_tcp.accept()
-        messaggio=server_tcp.recv(4096).decode('utf-8')
-
+        client, address = server_tcp.accept()
+        
+        messaggio = client.recv(4096).decode('utf-8')
         while messaggio != "end":
-
-            
-            print(f"Connessione da {address}")
-            messaggio=client.recv(4096).decode('utf-8')  
             messaggio = messaggio.split(sep=",")
-            func =int(messaggio[0])
+            func = int(messaggio[0])
             power = int(messaggio[1])
             if func == 1:
-                messaggio="forwrd"
-            elif func==2:
-                messaggio="backword"
+                messaggio = f"forward, con potenza {power}"
+            elif func == 2:
+                messaggio = f"backward, con potenza {power}"
             elif func == 3:
-                messaggio="right"
+                messaggio = f"right, con potenza {power}"
             elif func == 4:
-                messaggio = "left"
+                messaggio = f"left, con potenza {power}"
             else:
-                print("error")
-            
+                messaggio = "error"
             client.send(messaggio.encode('utf-8'))
-            messaggio=server_tcp.recv(4096).decode('utf-8')
-
+            messaggio = client.recv(4096).decode('utf-8')
 except KeyboardInterrupt:
-    print("\nProgramma interrotto da CTRL + C")
+    print("Chiusura del socket...")
 
-
-print("Chiusura della connessione...")
 server_tcp.close()
