428ad4784943f51152c77906a0f214c9b76c2c5b
Davide Delogu <davide.delogu@itiscuneo.eu>
2024-09-27 13:27:59 +0200

    ServerClientTcpAlphaBotCommentato

--- Client.py
+++ Client.py
@@ -1,13 +1,17 @@
+# Importazione della libreria socket
 import socket as s
 
+# Definizione dell'indirizzo del server
 server_tcp_address = ("10.210.0.49", 12345)
 
+# Creazione del socket TCP del client
 client_tcp = s.socket(s.AF_INET, s.SOCK_STREAM)
 
 try:
-    
+    # Connessione al server
     client_tcp.connect(server_tcp_address)
     
+    # Stampa delle istruzioni per l'utente
     print("""in questo programma gestirai la posizione virtuale di un robot,
     per gestirla devi utilizzare le funzioni:
     1 - forward
@@ -16,16 +20,29 @@ try:
     4 - left
     per uscire dalla connessione scrivi 'end'""")
     
+    # Input dell'utente per la funzione e la potenza
     messaggio=input("Inserisci il numero della funzione e la potenza tra 0-100(num, power):  ")   
-   
+    
+    # Ciclo per inviare i comandi al server
     while messaggio!="end":
+        # Invio del messaggio al server
         client_tcp.send(messaggio.encode('utf-8'))
+        
+        # Ricezione della risposta dal server
         messaggio = client_tcp.recv(4096).decode('utf-8')
+        
+        # Stampa della risposta del server
         print(f"Il server dice: {messaggio}")
+        
+        # Input dell'utente per la funzione e la potenza
         messaggio=input("Inserisci il numero della funzione e la potenza tra 0-100(num, power):  ")
 
 except KeyboardInterrupt:
+    # Stampa di un messaggio di interruzione del programma
     print("\nProgramma interrotto da CTRL + C")
 
+# Stampa di un messaggio di chiusura della connessione
 print("Chiusura della connessione...")
+
+# Chiusura del socket del client
 client_tcp.close()
--- Server.py
+++ Server.py
@@ -1,21 +1,37 @@
+# Importazione della libreria socket
 import socket as s
 
+# Creazione del socket TCP
 server_tcp = s.socket(s.AF_INET, s.SOCK_STREAM)
 
+# Definizione dell'indirizzo del server
 server_tcp_address = ("10.210.0.164", 12345)
 
+# Associazione del socket all'indirizzo del server
 server_tcp.bind(server_tcp_address)
 
+# Attivazione della modalità di ascolto del socket
 server_tcp.listen(1)
+
 try:
+    # Ciclo infinito per accettare le connessioni dei client
     while True:
+        # Accettazione della connessione del client
         client, address = server_tcp.accept()
         
+        # Ricezione del messaggio dal client
         messaggio = client.recv(4096).decode('utf-8')
+        
+        # Ciclo per elaborare i comandi ricevuti dal client
         while messaggio != "end":
+            # Suddivisione del messaggio in due parti separate da una virgola
             messaggio = messaggio.split(sep=",")
+            
+            # Estrazione della funzione e della potenza dal messaggio
             func = int(messaggio[0])
             power = int(messaggio[1])
+            
+            # Elaborazione del comando ricevuto
             if func == 1:
                 messaggio = f"forward, con potenza {power}"
             elif func == 2:
@@ -26,9 +42,15 @@ try:
                 messaggio = f"left, con potenza {power}"
             else:
                 messaggio = "error"
+            
+            # Invio della risposta al client
             client.send(messaggio.encode('utf-8'))
+            
+            # Ricezione del nuovo messaggio dal client
             messaggio = client.recv(4096).decode('utf-8')
 except KeyboardInterrupt:
+    # Stampa di un messaggio di chiusura del socket in caso di interruzione
     print("Chiusura del socket...")
 
+# Chiusura del socket
 server_tcp.close()
